888dbeb4379c0841d3e2ea235d8c89ded1e52495
gabrielrcosta1 <gabriel.ribeiro@burh.com.br>
2025-08-11 20:04:43 -0300

    feat: added DTO in login

--- app/Actions/Auth/AuthLoginAction.php
+++ app/Actions/Auth/AuthLoginAction.php
@@ -2,7 +2,18 @@
 
 namespace App\Actions\Auth;
 
+use App\DTOs\Auth\AuthLoginDTO;
+use Illuminate\Support\Facades\Auth;
+
 class AuthLoginAction
 {
-
+    public function handle(AuthLoginDTO $credentials)
+    {
+        if (!Auth::attempt($credentials->toArray())) {
+            return false;
+        }
+        $user = Auth::user();
+        $user->tokens()->delete();
+        return $user->createToken("auth_token")->plainTextToken;
+    }
 }
